ba748916e96c3945545b4dac11c3da1b92187645
nextgenprogrammer <84678682+nextgenprogrammer@users.noreply.github.com>
2022-04-18 17:41:27 +0530

    feature-toggle

--- JS/animate.js
+++ JS/animate.js
@@ -2,7 +2,23 @@ function hamMenu() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
+      visibilityToggle("comp-l1vzp300_video")
+      visibilityToggle("text-line1")
+      visibilityToggle("text-line2")
     } else {
       x.className = "topnav";
+      visibilityToggle("comp-l1vzp300_video")
+      visibilityToggle("text-line1")
+      visibilityToggle("text-line2")
     }
   }
+
+
+function visibilityToggle(element) {
+  var x = document.getElementById(element);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
